c7b5808e4803885a4dbc28f6b290d13dc02998e8
alidrisy <a.alidrisy6@gmail.com>
2023-10-06 01:45:47 +0300

    fix it

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,7 +15,7 @@ echo "<html>
     Holberton School
   </body>
 </html>
-" | > '/data/web_static/releases/test/index.html'
+" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
 sudo sed -i '38i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
