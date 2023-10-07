e334f9d88c57267bd294800ddcd98aef1e0a5005
alidrisy <a.alidrisy6@egamil.com>
2023-10-08 01:10:05 +0300

    fix it

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,10 +8,10 @@ def do_pack():
     """ generates a .tgz archive from the contents of the web_static folder
     of your AirBnB Clone repo, using the function do_pack."""
     t = datetime.now()
-    name = f"web_static_{t.year}{t.month}{t.day}{t.hour}{t.minute}\
-{t.second}.tgz"
+    name = "web_static_{}{}{}{}{}{}.tgz"\
+            .format(t.year, t.month, t.day, t.hour, t.minute, t.second)
     local("mkdir -p versions")
     com = local(f"tar -cvzf versions/{name} web_static")
     if com.failed is True:
         return (None)
-    return (archive_path)
+    return (f"versions/{name} web_static")
