03cc5d218902863ea094eedb572ebe2d7d32d3d1
alidrisy <a.alidrisy6@egamil.com>
2023-10-08 01:23:37 +0300

    fix style

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,11 +7,12 @@ from datetime import datetime
 def do_pack():
     """ generates a .tgz archive from the contents of the web_static folder
     of your AirBnB Clone repo, using the function do_pack."""
-    t = datetime.now()
-    name = "web_static_{}{}{}{}{}{}.tgz"\
-            .format(t.year, t.month, t.day, t.hour, t.minute, t.second)
+
+    t = datetime.now().strftime("%Y%m%d%H%M%S")
+    name = "web_static_{}.tgz".format(t)
     local("mkdir -p versions")
     com = local(f"tar -cvzf versions/{name} web_static")
+
     if com.failed is True:
         return (None)
     return (f"versions/{name}")
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Fabric script that generates a .tgz archive """
+""" Fabric script that generates a .tgz archive  and """
 from fabric.api import local
 from datetime import datetime
 
@@ -7,12 +7,16 @@ from datetime import datetime
 def do_pack():
     """ generates a .tgz archive from the contents of the web_static folder
     of your AirBnB Clone repo, using the function do_pack."""
+    
     t = datetime.now()
     name = "web_static_{}{}{}{}{}{}.tgz"\
             .format(t.year, t.month, t.day, t.hour, t.minute, t.second)
     local("mkdir -p versions")
     com = local(f"tar -cvzf versions/{name} web_static")
+
     if com.failed is True:
         return (None)
     return (f"versions/{name}")
+
+
 
