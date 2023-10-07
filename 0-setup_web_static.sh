8173397b103dffe3fdeb855f5ecd38b491f7a8b1
alidrisy <a.alidrisy6@gmail.com>
2023-10-07 23:24:26 +0300

    fix code

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 # sets up web servers for the deployment of web_static.
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
+ngin=$(pgrep -f 'nginx')
+if [ -z "$ngin" ]; then
+	sudo apt-get -y update
+	sudo apt-get -y upgrade
+	sudo apt-get -y install nginx
+fi
 
-sudo mkdir -p '/data/web_static/releases/test/'
-sudo mkdir -p '/data/web_static/shared/'
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo touch /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 echo "<html>
   <head>
@@ -16,8 +21,9 @@ echo "<html>
   </body>
 </html>
 " > /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
-sudo sed -i '38i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-
+con=$(grep 'location /hbnb_static' /etc/nginx/sites-available/default)
+if [ -z "$con" ]; then
+	sudo sed -i '38i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-available/default
+fi
 sudo service nginx restart
