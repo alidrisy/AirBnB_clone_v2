f17e7a190ccc249fc10fc0ff2b67153a1902e6ed
alidrisy <a.alidrisy6@egamil.com>
2023-10-09 02:01:15 +0300

    fix it

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -20,7 +20,7 @@ def do_clean(number=0):
     [list1.pop() for i in range(number)]
     [local("rm versions/{}".format(a)) for a in list1]
 
-    list1 = run("ls -tr").split()
+    list1 = run("ls -tr /data/web_static/releases").split()
     list1 = [a for a in list1 if "web_static_" in a]
     [list1.pop() for i in range(number)]
     [run("rm -rf ./{}".format(a)) for a in list1]
