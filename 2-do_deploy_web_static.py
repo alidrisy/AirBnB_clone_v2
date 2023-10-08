c262da1187dcdb5679dae12a7fcc384bb1cd6133
alidrisy <a.alidrisy6@egamil.com>
2023-10-08 21:33:00 +0300

    fix it

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,47 +1,55 @@
 #!/usr/bin/python3
-""" Fabric script that generates a .tgz archive  and distributes it
-to server """
-from fabric.api import put, run, env
-from datetime import datetime
+
+"""Script (based on the file 1-pack_web_static.py) that distributes
+    an archive to your web servers, using the function do_deploy
+"""
+
+from fabric.api import env, put, run
 import os
 
-env.hosts = ["107.23.100.114", "35.153.17.86"]
+env.hosts = [
+            '54.160.79.52',
+            '52.86.196.120'
+        ]
+
 env.user = "ubuntu"
 env.key_filename = "~/.ssh/id_rsa"
 
 
 def do_deploy(archive_path):
-    """ distributes an archive to your web servers """
-
+    """Function to distribute an archive to web servers
+    """
     if not os.path.exists(archive_path):
         return (False)
 
-    dir1 = archive_path.split("/")[-1].split(".")[0]
-    file1 = f"{dir1}.tgz"
-    path = "/data/web_static/releases/{}".format(dir1)
+    archive_name = archive_path.split('/')[-1]
+    name = archive_name.split('.')[0]
+    uncompress_path = "/data/web_static/releases/{}".format(name)
+    uncompress_cmd = "sudo tar -xzf /tmp/{} -C {}"\
+                     .format(archive_name, uncompress_path)
+    create_path = "sudo mkdir -p {}".format(uncompress_path)
+    remove_archive = "sudo rm -rf /tmp/{}".format(archive_name)
+    move = "sudo mv {}/web_static/* {}"\
+           .format(uncompress_path, uncompress_path)
+    remove_web_static = "sudo rm -rf {}/web_static".format(uncompress_path)
+    link_archive = "sudo ln -s {} /data/web_static/current"\
+                   .format(uncompress_path)
 
-    check = put(archive_path, "/tmp/")
-    if check.failed is True:
+    if put(archive_path, '/tmp/').failed is True:
         return (False)
-    check = run(f"sudo mkdir -p {path}")
-    if check.failed is True:
+    if run(create_path).failed is True:
         return (False)
-    check = run(f"sudo tar -xzf /tmp/{file1} -C {path}")
-    if check.failed is True:
+    if run(uncompress_cmd).failed is True:
         return (False)
-    check = run(f"sudo rm -rf /tmp/{file1}")
-    if check.failed is True:
+    if run(remove_archive).failed is True:
         return (False)
-    check = run(f"sudo mv {path}/web_static/* {path}")
-    if check.failed is True:
+    if run(move).failed is True:
         return (False)
-    check = run(f"sudo rm -rf {path}/web_static/")
-    if check.failed is True:
+    if run(remove_web_static).failed is True:
         return (False)
-    check = run(f"sudo rm -rf /data/web_static/current")
-    if check.failed is True:
+    if run("sudo rm -rf /data/web_static/current").failed is True:
         return (False)
-    check = run(f"sudo ln -s {path} /data/web_static/current")
-    if check.failed is True:
+    if run(link_archive).failed is True:
         return (False)
+    print("New version deployed!")
     return (True)
