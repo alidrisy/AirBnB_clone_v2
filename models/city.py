587da1efd9a200daa5f4d020a71939bd815f1ffd
alidrisy <a.alidrisy6@egamil.com>
2023-10-20 22:04:11 +0300

    8. List of states

--- models/city.py
+++ models/city.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column, ForeignKey
 from models.place import Place
@@ -14,3 +15,7 @@ class City(BaseModel, Base):
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     places = relationship('Place', cascade='all, delete, delete-orphan',
                           backref='cities')
+
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
