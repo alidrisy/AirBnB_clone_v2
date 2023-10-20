26ba6550c04a965a3e49000e8ee128d594dd4918
alidrisy <a.alidrisy6@egamil.com>
2023-10-20 22:17:56 +0300

    8. List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -20,7 +20,7 @@ def states_list():
     UL tag: with the list of all State objects present in DBStorage sorted
     by name (A->Z) tip
     LI tag: description of one State: <state.id>: <B><state.name></B> """
-    states = storage.all('State')
+    states = list(storage.all('State').values())
     return render_template("7-states_list.html", states=states)
 
 
