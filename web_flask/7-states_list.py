020f63d1b71abd131f8d46a37317ec26110a33a9
alidrisy <a.alidrisy6@egamil.com>
2023-10-21 00:01:45 +0300

    fix it

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,10 +4,9 @@ from flask import Flask, render_template
 from models import storage
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route("/states_list")
+@app.route("/states_list", strict_slashes = False)
 def states_list():
     """ Display a HTML page with list of states """
     states = sorted(list(storage.all("State").values()), key=lambda s: s.name)
