04fde0f8f5a865db6ca2139ffd18b6739a883e4b
alidrisy <a.alidrisy6@egamil.com>
2023-10-21 16:28:11 +0300

    10. States and State

---
+++ web_flask/9-states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Flask web application """
+
+from flask import Flask, render_template
+from models import *
+from models import storage
+app = Flask(__name__)
+
+
+@app.route('/states/', defaults={'id': None}, strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def states(id):
+    """Display a HTML page with list states or state and his cities """
+    dict_states = storage.all("State")
+    if id:
+        k = f"State.{id}"
+        state = dict_states[k]
+        return render_template('9-states.html', state=state)
+    states = dict_states.values()
+    return render_template('9-states.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
