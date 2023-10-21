a1d965a17afba38be9c304c198d1ca4634d68d20
alidrisy <a.alidrisy6@egamil.com>
2023-10-21 11:21:07 +0300

    fix error

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 """ Flask web application """
-from flask import Flask, render_template
-from models import storage
 
+from flask import Flask, render_template
+from models import *
+from models import storage
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
 def states_list():
-    """ Display a HTML page with list of states """
-    states = sorted(list(storage.all("State").values()), key=lambda s: s.name)
+    """Display a HTML page with list states """
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown(exc):
+def teardown_db(exception):
     """ Remove the current SQLAlchemy Session """
     storage.close()
 
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
